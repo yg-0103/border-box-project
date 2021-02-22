ef92ca249a25346312ca277007b2038804ce7fdd
wisemett <frontendkid@gmail.com>
2021-02-22 17:13:50 +0900

    Markup, Styling and Script for Completed Section.

--- client/src/ts/main.ts
+++ client/src/ts/main.ts
@@ -4,7 +4,10 @@ const todayYear = new Date().getFullYear();
 const todayMonth = new Date().getMonth() + 1;
 const todayDate = new Date().getDate();
 
-const today = `${todayYear}${("0" + todayMonth).slice(-2)}${("0" + (todayDate-1)).slice(-2)}`
+const today = `${todayYear}${('0' + todayMonth).slice(-2)}${(
+  '0' +
+  (todayDate - 1)
+).slice(-2)}`;
 // console.log(today);
 
 // const getMovieList = async () => {
@@ -18,8 +21,4 @@ let currentSlide = 1;
 const $prevBtn = document.querySelector('.prev');
 const $nextBtn = document.querySelector('.next');
 
-$prevBtn.onclick = () => {
-  currentSlide += 1;
-  
-}
 calender();
