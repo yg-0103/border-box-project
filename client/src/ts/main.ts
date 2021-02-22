f9b09915020782bac8065db3837527c78378baf7
yg-0103 <rladusrn89@gmail.com>
2021-02-22 17:18:04 +0900

    feat: calendar edit

--- client/src/ts/main.ts
+++ client/src/ts/main.ts
@@ -4,7 +4,7 @@ const todayYear = new Date().getFullYear();
 const todayMonth = new Date().getMonth() + 1;
 const todayDate = new Date().getDate();
 
-const today = `${todayYear}${("0" + todayMonth).slice(-2)}${("0" + (todayDate-1)).slice(-2)}`
+const today = `${todayYear}${('0' + todayMonth).slice(-2)}${('0' + (todayDate - 1)).slice(-2)}`;
 // console.log(today);
 
 // const getMovieList = async () => {
@@ -13,13 +13,9 @@ const today = `${todayYear}${("0" + todayMonth).slice(-2)}${("0" + (todayDate-1)
 // };
 // getMovieList();
 
-let currentSlide = 1;
+const currentSlide = 1;
 
 const $prevBtn = document.querySelector('.prev');
 const $nextBtn = document.querySelector('.next');
 
-$prevBtn.onclick = () => {
-  currentSlide += 1;
-  
-}
 calender();
