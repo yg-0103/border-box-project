f16186c5a109e3da2be4c9a947e4f71e8d27d1a2
wisemett <frontendkid@gmail.com>
2021-02-23 10:26:08 +0900

    feat: myBooking section

--- client/src/ts/myBooking.ts
+++ client/src/ts/myBooking.ts
@@ -1,37 +1,49 @@
 // import renderCompleted from './completed';
+const $btnMyBooking = document.querySelector(
+  '.btn-myBooking'
+) as HTMLButtonElement;
+const $myBooking = document.querySelector('.myBooking') as HTMLElement;
+const $myBookingForm = document.querySelector(
+  '.myBooking_form'
+) as HTMLFormElement;
+const $myBookingInput = document.getElementById(
+  'myBooking_input'
+) as HTMLFormElement;
+const $invalid = document.querySelector('.invalid') as HTMLElement;
+const $closeBtn = document.querySelector(
+  '.myBooking_close'
+) as HTMLButtonElement;
+
+const cleanInput = () => {
+  $myBookingInput.value = '';
+};
+
+const closeMybooking = () => {
+  $myBooking.classList.remove('active');
+  cleanInput();
+};
 
 const myBooking = (() => {
-  const $btnMyBooking = document.querySelector(
-    '.btn-myBooking'
-  ) as HTMLButtonElement;
-  const $myBooking = document.querySelector('.myBooking') as HTMLElement;
-  const $myBookingForm = document.querySelector(
-    '.myBooking_form'
-  ) as HTMLFormElement;
-  const $myBookingInput = document.getElementById(
-    'myBooking_input'
-  ) as HTMLFormElement;
-  const $closeBtn = document.querySelector(
-    '.myBooking_close'
-  ) as HTMLButtonElement;
-
   $btnMyBooking.addEventListener('click', () => {
     $myBooking.classList.add('active');
   });
-  $closeBtn.addEventListener('click', () => {
-    $myBooking.classList.remove('active');
-    $myBookingInput.value = '';
-  });
+  $closeBtn.addEventListener('click', closeMybooking);
 
-  $myBookingForm.addEventListener('submit', e => {
+  $myBookingForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    $myBooking.classList.remove('active');
+
+    if (!/\d{8}[A-C]/.test($myBookingInput.value)) {
+      $invalid.style.display = 'inline-block';
+      cleanInput();
+      return;
+    }
+
     /*
     $myBookingInput.value로 서버에서 해당 예매 정보 검색
     -> 찾은 객체를 인수로 completed의 render 함수 실행
     renderCompleted()
     */
-    $myBookingInput.value = '';
+    closeMybooking();
   });
 
   return () => {};
