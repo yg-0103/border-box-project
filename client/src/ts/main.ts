407e85a042bfe212ca3c9a6f9d62951cd7a2fc23
yg-0103 <rladusrn89@gmail.com>
2021-02-23 10:12:01 +0900

    feat: goToTop scroll

--- client/src/ts/main.ts
+++ client/src/ts/main.ts
@@ -1,5 +1,7 @@
 import reservation from './reservationEventHandle';
 import myBooking from './myBooking';
+import scrollGoToTop from './scrollGoToTop';
 
+scrollGoToTop();
 reservation();
 myBooking();
